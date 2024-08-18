dcfde780ed4a13594d4f0a5269cad6907df44fbf
DevnDSA <132866106+JustDevnDSA@users.noreply.github.com>
2024-08-18 21:00:41 +0530

    Add files via upload

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 const Navbar = () => {
   return (
     <>
-      <div className="flex w-full lg:px-28 xl:px-32 2xl:px-36 md:px-24 mt-3 mx-auto  py-2 items-center justify-between flex-col gap-3.5 md:flex-row md:gap-0   fixed top-0 z-20">
+      <div className="flex w-full lg:px-28 xl:px-32 2xl:px-36 md:px-24 mt-3 mx-auto  py-2 items-center justify-between flex-col gap-3.5 md:flex-row md:gap-0   fixed top-0 z-20 bg-gray-950 border-b border-gray-700 border-opacity-80 pb-4 ">
         <Logo />
         <div>
           <Link to={"/addBookmark"}>
@@ -13,7 +13,6 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
-      <hr className="w-full  fixed z-20 mt-[9rem] md:mt-20 opacity-15" />
     </>
   );
 };
